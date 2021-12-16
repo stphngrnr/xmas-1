0d1d3d42cd942ff55f57dc2cc56e2173514afd55
geblix3000 <43614984+geblix3000@users.noreply.github.com>
2021-12-16 13:48:06 +0100

    Update xmas-cc.sh

--- xmas-cc.sh
+++ xmas-cc.sh
@@ -58,7 +58,7 @@ while true; do
         color=$(((color+1)%8))
         # Flashing text
         sh=1
-        for l in OPS A L T A A A
+        for l in O P S - A L T A A A
         do
             tput cup $((lin+1)) $((c+sh))
             echo $l
